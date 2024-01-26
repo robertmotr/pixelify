c09116fadbf7689538fc4bfa2eb1450e3513645e
Robert Motrogeanu <robertmotrogeanu@yahoo.ca>
2024-01-26 02:05:37 -0500

    rewrote filter class for the n-th time today

--- include/filter.h
+++ include/filter.h
@@ -6,41 +6,35 @@
 #include <string.h>
 #include <cstring>
 
-typedef struct basic_filter_arrays {
-    const int dimension =                                                           3;
-    const int identity_filter_data[9] =                                             {0, 0, 0, 0, 1, 0, 0, 0, 0};
-    const int box_blur_filter_data[9] =                                             {1, 1, 1, 1, 1, 1, 1, 1, 1};
-    const int gaussian_blur_filter_data[9] =                                        {1, 2, 1, 2, 4, 2, 1, 2, 1};
-    const int sharpen_filter_data[9] =                                              {0, -1, 0, -1, 5, -1, 0, -1, 0};
-    const int edge_detection_filter_data[9] =                                       {-1, -1, -1, -1, 8, -1, -1, -1, -1};
-    const int emboss_filter_data[9] =                                               {-2, -1, 0, -1, 1, 1, 0, 1, 2};
-    const int sobel_filter_data[9] =                                                {-1, 0, 1, -2, 0, 2, -1, 0, 1}; 
-} basic_filter_arrays;
+#define FILTER_DIMENSION 3
+
+extern const int* identity_filter_data;
+extern const int* edge_filter_data;
+extern const int* sharpen_filter_data;
+extern const int* box_blur_filter_data;
+extern const int* gaussian_blur_filter_data;
+extern const int* unsharp_mask_filter_data;
+extern const int* high_pass_filter_data;
+extern const int* emboss_filter_data;
+extern const int* sobel_filter_data;
+extern const int* laplacian_filter_data;
+extern const int* motion_blur_filter_data;
+
+extern const filter identity_filter;
+extern const filter edge_filter;
+extern const filter sharpen_filter;
+extern const filter box_blur_filter;
+extern const filter gaussian_blur_filter;
+extern const filter unsharp_mask_filter;
+extern const filter high_pass_filter;
+extern const filter emboss_filter;
+extern const filter sobel_filter;
+extern const filter laplacian_filter;
+extern const filter motion_blur_filter;
+
+extern const std::vector<const int*> basic_filter_data;
 
 class filter {
-private:
-    basic_filter_arrays basic_filters;
-
-    int *find_basic_filter(const char *str) {
-        if (strcmp(str, "Identity") == 0) {
-            return (int *)basic_filters.identity_filter_data;
-        } else if (strcmp(str, "Box Blur") == 0) {
-            return (int *)basic_filters.box_blur_filter_data;
-        } else if (strcmp(str, "Gaussian Blur") == 0) {
-            return (int *)basic_filters.gaussian_blur_filter_data;
-        } else if (strcmp(str, "Sharpen") == 0) {
-            return (int *)basic_filters.sharpen_filter_data;
-        } else if (strcmp(str, "Edge Detection") == 0) {
-            return (int *)basic_filters.edge_detection_filter_data;
-        } else if (strcmp(str, "Emboss") == 0) {
-            return (int *)basic_filters.emboss_filter_data;
-        } else if (strcmp(str, "Sobel") == 0) {
-            return (int *)basic_filters.sobel_filter_data;
-        } else {
-            return nullptr;
-        }
-    }
-
 public:
 
     char*                           filter_name;
@@ -60,6 +54,37 @@ public:
     filter(const char* name, int *data, unsigned int dimension)
         : filter_dimension(dimension), filter_data(data) {
         size_t size = strlen(name) + 1;
+
+        if(filter_name != nullptr) {
+            delete[] filter_name;
+            filter_name = nullptr;
+        }
+
+        filter_name = new char[size];
+        strcpy(filter_name, name);
+        this->name_size = size;
+    }
+
+    // constructor with just name
+    filter(const char* name)
+        : filter_dimension(0), filter_data(nullptr) {
+        size_t size = strlen(name) + 1;
+
+        filter_name = new char[size];
+        strcpy(filter_name, name);
+        this->name_size = size;
+    }
+
+    // basic filter initializer
+    filter(const char* name, const int* data, unsigned int dimension)
+        : filter_dimension(dimension) {
+        size_t size = strlen(name) + 1;
+
+        if(filter_name != nullptr) {
+            delete[] filter_name;
+            filter_name = nullptr;
+        }
+
         filter_name = new char[size];
         strcpy(filter_name, name);
         this->name_size = size;
@@ -69,6 +94,12 @@ public:
     filter(const char* name, unsigned int dimension)
         : filter_dimension(dimension), filter_data(new int[dimension * dimension]) {
         size_t size = strlen(name) + 1;
+
+        if(filter_name != nullptr) {
+            delete[] filter_name;
+            filter_name = nullptr;
+        }
+
         filter_name = new char[size];
         strcpy(filter_name, name);
         this->name_size = size;
@@ -108,128 +139,30 @@ public:
             delete[] filter_name;
             filter_name = nullptr;
         }
-        if (filter_data != nullptr) {
-            delete[] filter_data;
-            filter_data = nullptr;
-        }
-        filter_name = new char[other.name_size];
-        strcpy(filter_name, other.filter_name);
-        filter_dimension = other.filter_dimension;
-        filter_data = new int[filter_dimension * filter_dimension];
-        memcpy(filter_data, other.filter_data, filter_dimension * filter_dimension * sizeof(int));
-        return *this;
-    }
-
-    // filter strength on an image is a function of the filters size relative to the images size
-    // expand_filter takes in percentage [0, 100], and expands the filter to the largest rectangle that can fit
-    // within the image, as a percentage of the image size
-    // i.e if percentage = 1, then the filter will be expanded to the largest rectangle that can fit within 1% of the image
-    // returns true on success, false on failure
-    bool expand_filter(unsigned char percentage, unsigned int image_width, unsigned int image_height) {
-        if(percentage > 100) {
-            return false;
-            // since unsigned, percentage/width/height can't be negative
-        }
-
-        double desired_area = (percentage / 100.0) * image_width * image_height;
-
-        // Initialize variables to store the best result
-        double best_diff = std::numeric_limits<double>::infinity();
-        unsigned int best_dimension = 0;
-
-        // Iterate over all possible rectangle sizes
-        for (unsigned int dim = 3; dim < std::min(image_width, image_height); dim += 3) {
-        
-            double current_area = dim * dim;
-
-            // difference between the current area and the desired area
-            double diff = std::abs(current_area - desired_area);
-
-            // update best result if the current is closer to desired
-            if (diff < best_diff) {
-                best_diff = diff;
-                best_dimension = dim;
+        if (filter_data != other.filter_data) {
+            if(filter_data != nullptr) {
+                delete[] filter_data;
+                filter_data = nullptr;
             }
+            filter_data = new int[other.filter_dimension * other.filter_dimension]; 
+            memcpy(filter_data, other.filter_data, other.filter_dimension * other.filter_dimension * sizeof(int));
         }
-
-        // it could be possible best_dimension > image_width/image_height
-        // in that case scale it back a bit such that it still is within the image and
-        // is divisible by 3x3 squares
-        while((best_dimension > image_width ||
-                best_dimension > image_height ||
-                best_dimension % 3 != 0) && 
-                best_dimension > 3) {
-            best_dimension -= 3;
-        }
-        assert(best_dimension % 3 == 0 && best_dimension > 0);
-        assert(best_dimension <= image_width && best_dimension <= image_height);
-
-        int *copy_data = find_basic_filter(filter_name);
-
-        int *new_filter_data = new int[best_dimension * best_dimension];
-        for(int i = 0; i < best_dimension * best_dimension; i += 9) {
-            memcpy(new_filter_data + i, copy_data, basic_filters.dimension * basic_filters.dimension * sizeof(int));
-        }
-
-        delete[] filter_data;
-
-        filter_dimension = best_dimension;
-        filter_data = new_filter_data;
-
-        return true;
+        
+        return *this;
     }
 };
 
-inline std::vector<filter> get_filter_list() {
+const int* find_basic_filter_data(const char *name);
+const filter find_basic_filter(const char *name);
 
-    int *IDENTITY_FILTER_DATA = new int[9]{0, 0, 0, 0, 1, 0, 0, 0, 0};
-    int *BOX_BLUR_FILTER_DATA = new int[9]{1, 1, 1, 1, 1, 1, 1, 1, 1};
-    int *GAUSSIAN_BLUR_FILTER_DATA = new int[9]{1, 2, 1, 2, 4, 2, 1, 2, 1};
-    int *SHARPEN_FILTER_DATA = new int[9]{0, -1, 0, -1, 5, -1, 0, -1, 0};
-    int *EDGE_DETECTION_FILTER_DATA = new int[9]{-1, -1, -1, -1, 8, -1, -1, -1, -1};
-    int *EMBOSS_FILTER_DATA = new int[9]{-2, -1, 0, -1, 1, 1, 0, 1, 2};
-    int *SOBEL_FILTER_DATA = new int[9]{-1, 0, 1, -2, 0, 2, -1, 0, 1};
+filter *create_filter_from_strength(const char *name, unsigned int image_width,
+ unsigned int image_height, unsigned char percentage);
 
-    filter identity_filter("Identity", IDENTITY_FILTER_DATA, 3);
-    filter box_blur_filter("Box Blur", BOX_BLUR_FILTER_DATA, 3);
-    filter gaussian_blur_filter("Gaussian Blur", GAUSSIAN_BLUR_FILTER_DATA, 3);
-    filter sharpen_filter("Sharpen", SHARPEN_FILTER_DATA, 3);
-    filter edge_detection_filter("Edge Detection", EDGE_DETECTION_FILTER_DATA, 3);
-    filter emboss_filter("Emboss", EMBOSS_FILTER_DATA, 3);
-    filter sobel_filter("Sobel", SOBEL_FILTER_DATA, 3);
-    filter null = filter();
-
-    std::vector<filter> filter_list = {
-        identity_filter,
-        box_blur_filter,
-        gaussian_blur_filter,
-        sharpen_filter,
-        edge_detection_filter,
-        emboss_filter,
-        sobel_filter,
-        null
-    };
-
-    return filter_list;
-}
-
-inline filter create_filter_from_strength(const char *name, unsigned int image_width,
- unsigned int image_height, unsigned char percentage) {
-
-    filter returnable;
-    std::vector<filter> filter_list = get_filter_list();
-    for (unsigned int i = 0; i < filter_list.size(); i++) {
-        if (strcmp(filter_list[i].filter_name, name) == 0) {
-            returnable = filter_list[i];
-            break;
-        }
-    }
-
-    if(returnable.expand_filter(percentage, image_width, image_height)) {
-        return returnable;
-    } else {
-        return filter(); // error
-    }
-}
-
+// filter strength on an image is a function of the filters size relative to the images size
+// expand_filter takes in percentage [0, 100], and expands the filter to the largest rectangle that can fit
+// within the image, as a percentage of the image size
+// i.e if percentage = 1, then the filter will be expanded to the largest rectangle that can fit within 1% of the image
+// returns true on success, false on failure
+bool expand_filter(unsigned char percentage, unsigned int image_width, unsigned int image_height, 
+    const char *basic_filter_name, filter *destination);
 #endif // __FILTER__H__
---
+++ src/filter.cu
@@ -0,0 +1,185 @@
+#include "filter.h"
+
+const int* identity_filter_data = new int[9]{
+    0, 0, 0,
+    0, 1, 0,
+    0, 0, 0
+};
+const int* edge_filter_data = new int[9]{
+    0, 1, 0,
+    1, -4, 1,
+    0, 1, 0
+};
+const int* sharpen_filter_data = new int[9]{
+    0, -1, 0,
+    -1, 5, -1,
+    0, -1, 0
+};
+const int* box_blur_filter_data = new int[9]{
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1
+};
+const int* gaussian_blur_filter_data = new int[9]{
+    1, 2, 1,
+    2, 4, 2,
+    1, 2, 1
+};
+const int* unsharp_masking_filter_data = new int[9]{
+    -1, -1, -1,
+    -1, 9, -1,
+    -1, -1, -1
+};
+const int* high_pass_filter_data = new int[9]{
+    0, -1, 0,
+    -1, 5, -1,
+    0, -1, 0
+};
+const int* emboss_filter_data = new int[9]{
+    -2, -1, 0,
+    -1, 1, 1,
+    0, 1, 2
+};
+const int* laplacian_filter_data = new int[9]{
+    0, -1, 0,
+    -1, 4, -1,
+    0, -1, 0
+};
+const int* motion_blur_filter_data = new int[9]{
+    1, 0, 0,
+    0, 1, 0,
+    0, 0, 1
+};
+
+const std::vector<const int*> basic_filter_data = {
+    identity_filter_data,
+    edge_filter_data,
+    sharpen_filter_data,
+    box_blur_filter_data,
+    gaussian_blur_filter_data,
+    unsharp_masking_filter_data,
+    high_pass_filter_data,
+    emboss_filter_data,
+    laplacian_filter_data,
+    motion_blur_filter_data
+};
+
+const filter identity_filter = filter("Identity Filter", identity_filter_data, 3);
+const filter edge_filter = filter("Edge Filter", edge_filter_data, 3);
+const filter sharpen_filter = filter("Sharpen Filter", sharpen_filter_data, 3);
+const filter box_blur_filter = filter("Box Blur Filter", box_blur_filter_data, 3);
+const filter gaussian_blur_filter = filter("Gaussian Blur Filter", gaussian_blur_filter_data, 3);
+const filter unsharp_masking_filter = filter("Unsharp Masking Filter", unsharp_masking_filter_data, 3);
+const filter high_pass_filter = filter("High Pass Filter", high_pass_filter_data, 3);
+const filter emboss_filter = filter("Emboss Filter", emboss_filter_data, 3);
+const filter laplacian_filter = filter("Laplacian Filter", laplacian_filter_data, 3);
+const filter motion_blur_filter = filter("Motion Blur Filter", motion_blur_filter_data, 3);
+
+const std::vector<filter> basic_filters = {
+    identity_filter,
+    edge_filter,
+    sharpen_filter,
+    box_blur_filter,
+    gaussian_blur_filter,
+    unsharp_masking_filter,
+    high_pass_filter,
+    emboss_filter,
+    laplacian_filter,
+    motion_blur_filter
+};
+
+const int* find_basic_filter_data(const char *name) {
+    for(auto f : basic_filters) {
+        if(strcmp(f.filter_name, name) == 0) {
+            return f.filter_data;
+        }
+    }
+    return nullptr;
+}
+
+const filter find_basic_filter(const char *name) {
+    for(auto f : basic_filters) {
+        if(strcmp(f.filter_name, name) == 0) {
+            return f;
+        }
+    }
+    return filter(); // null filter
+}
+
+// filter strength on an image is a function of the filters size relative to the images size
+// expand_filter takes in percentage [0, 100], and expands the filter to the largest rectangle that can fit
+// within the image, as a percentage of the image size
+// i.e if percentage = 1, then the filter will be expanded to the largest rectangle that can fit within 1% of the image
+// returns true on success, false on failure
+bool expand_filter(unsigned char percentage, unsigned int image_width, unsigned int image_height, 
+    const char *basic_filter_name, filter *destination) {
+    if(percentage > 100) {
+        return false;
+        // since unsigned, percentage/width/height can't be negative
+    }
+    if(percentage == 0) {
+        return true;
+    }
+
+    double desired_area = (percentage / 100.0) * image_width * image_height;
+
+    // Initialize variables to store the best result
+    double best_diff = std::numeric_limits<double>::infinity();
+    unsigned int best_dimension = 0;
+
+    // Iterate over all possible rectangle sizes
+    for (unsigned int dim = 3; dim < std::min(image_width, image_height); dim += 3) {
+    
+        double current_area = dim * dim;
+
+        // difference between the current area and the desired area
+        double diff = std::abs(current_area - desired_area);
+
+        // update best result if the current is closer to desired
+        if (diff < best_diff) {
+            best_diff = diff;
+            best_dimension = dim;
+        }
+    }
+
+    // it could be possible best_dimension > image_width/image_height
+    // in that case scale it back a bit such that it still is within the image and
+    // is divisible by 3x3 squares
+    while((best_dimension > image_width ||
+            best_dimension > image_height ||
+            best_dimension % 3 != 0) && 
+            best_dimension > 3) {
+        best_dimension -= 3;
+    }
+    assert(best_dimension % 3 == 0 && best_dimension > 0);
+    assert(best_dimension <= image_width && best_dimension <= image_height);
+
+    const int *copy_data = find_basic_filter_data(basic_filter_name);
+
+    int *new_filter_data = new int[best_dimension * best_dimension];
+    for(int i = 0; i < best_dimension * best_dimension; i += 9) {
+        memcpy(new_filter_data + i, copy_data, FILTER_DIMENSION * FILTER_DIMENSION * sizeof(int));
+    }
+
+    *destination = filter(basic_filter_name, new_filter_data, best_dimension);
+
+    return true;
+}
+
+filter *create_filter_from_strength(const char *basic_filter_name, unsigned int image_width,
+    unsigned int image_height, unsigned char percentage) {
+    
+    if(percentage > 100 || percentage == 0) {
+        return nullptr;
+    }
+
+    filter *expanded_filter = new filter("Expanded filter");
+
+    if(expand_filter(percentage, image_width, image_height, basic_filter_name, expanded_filter)) {
+        return expanded_filter;
+    }
+    else {
+        return nullptr;
+    }
+
+}
