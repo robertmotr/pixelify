0cfd606e8bab4c5205d80ff9f6435225d3c84ca7
Robert Motrogeanu <robertmotrogeanu@yahoo.ca>
2024-01-31 12:32:28 -0500

    added back old get raw functions

--- include/pixel.h
+++ include/pixel.h
@@ -43,8 +43,33 @@ struct Pixel {
     Pixel(int value) : Pixel(value, value, value) {}
 };
 
+// dont use this doesnt work with imgui, kept this so tests are stable
 template<unsigned int channels>
-void pixel_to_raw_image(const Pixel<channels> *input, unsigned char *output, unsigned int size) {
+unsigned char* pixel_to_raw_image(const Pixel<channels> *input, unsigned int size) {
+    unsigned char *output = new unsigned char[channels * size];
+    for (unsigned int i = 0; i < size; i++) {
+        for (unsigned int j = 0; j < channels; j++) {
+            output[i * channels + j] = static_cast<unsigned char>(input[i].data[j]);
+        }
+    }
+    return output;
+}
+
+// dont use this doesnt work with imgui, kept this so tests are stable
+template<unsigned int channels>
+Pixel<channels>* raw_image_to_pixel(const unsigned char *input, Pixel<channels> *output, unsigned int size) {
+    Pixel<channels> *output = new Pixel<channels>[size];
+    for (unsigned int i = 0; i < size; i++) {
+        for (unsigned int j = 0; j < channels; j++) {
+            output[i].data[j] = static_cast<int>(input[i * channels + j]);
+        }
+    }
+    return output;
+}
+
+// use these two below instead
+template<unsigned int channels>
+void imgui_get_raw_image(const Pixel<channels> *input, unsigned char *output, unsigned int size) {
     for (unsigned int i = 0; i < size; i++) {
         for (unsigned int j = 0; j < channels; j++) {
             output[i * INTERNAL_CHANNEL_SIZE + j] = static_cast<unsigned char>(input[i].data[j]);
@@ -57,7 +82,7 @@ void pixel_to_raw_image(const Pixel<channels> *input, unsigned char *output, uns
 }
 
 template<unsigned int channels>
-void raw_image_to_pixel(const unsigned char *input, Pixel<channels> *output, unsigned int size) {
+void imgui_get_pixels(const unsigned char *input, Pixel<channels> *output, unsigned int size) {
     for (unsigned int i = 0; i < size; i++) {
         for (unsigned int j = 0; j < channels; j++) {
             output[i].data[j] = static_cast<int>(input[i * INTERNAL_CHANNEL_SIZE + j]);
