00336364fb631083bf1da2158ba32568870443d0
AlawadhiBOT <alawadhisan@gmail.com>
2024-07-08 04:53:17 +0400

    More testing to see if we get anywhere.

---
+++ alawadhiBOT_run_file.bash
@@ -0,0 +1,11 @@
+rm -r build
+mkdir build
+cd build
+
+# Set environment variables for GCC 11
+export CUDAHOSTCXX=/usr/bin/g++-11
+export CC=/usr/bin/gcc
+export CXX=/usr/bin/g++
+
+# Run CMake and Make
+cmake -DCMAKE_BUILD_TYPE=Debug .. && make -j$(nproc)
